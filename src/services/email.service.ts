800ab4887de56cce9b0bb2079c03187f1491082f
Steven <stevenrq8@gmail.com>
2025-09-26 23:52:29 -0500

    Se actualiza la configuración de `secure` para detección automática basada en el puerto del servidor SMTP

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: true, // true para 465, false para otros puertos
+  secure: Number(process.env.EMAIL_PORT) === 465,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
