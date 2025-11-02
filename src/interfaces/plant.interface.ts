6940978338ff888dc3fecda83bb9345c75fe18af
Steven <stevenrq8@gmail.com>
2025-11-02 14:21:00 -0500

    Eliminar propiedades innecesarias de descripción y cantidad en el modelo de planta

--- src/interfaces/plant.interface.ts
+++ src/interfaces/plant.interface.ts
@@ -2,8 +2,6 @@ import { Model } from "mongoose";
 
 export interface IPlantDocument extends Document {
   name: string;
-  description: string;
-  quantity: number;
 }
 
 export interface IPlantModel extends Model<IPlantDocument> {}
--- src/models/plant.model.ts
+++ src/models/plant.model.ts
@@ -4,8 +4,6 @@ import { IPlantDocument, IPlantModel } from "../interfaces/plant.interface";
 export const PlantSchema = new Schema<IPlantDocument, IPlantModel>(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
-    quantity: { type: Number, required: true },
   },
   {
     timestamps: true,
